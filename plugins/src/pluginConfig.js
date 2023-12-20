b72b00c456b84126fb041bae77787d8d18369494
armando.alonso <armando.alonso@ukg.com>
2023-12-20 17:11:53 -0500

    fix plugin and behavior comment for parameters values

--- plugins/src/pluginConfig.js
+++ plugins/src/pluginConfig.js
@@ -198,7 +198,7 @@ module.exports = {
             - "objinstancevar"
 
           // The default value of the parameter. Can be omitted if the type is not a string, number, any, boolean or combo.
-          value: "the default value of the parameter",
+          initialValue: "the default value of the parameter",
 
           // Only for type "combo"
           items: [
@@ -295,7 +295,7 @@ module.exports = {
             - "objinstancevar"
 
           // The default value of the parameter. Can be omitted if the type is not a string, number, any, boolean or combo.
-          value: "the default value of the parameter",
+          initialValue: "the default value of the parameter",
 
           // Only for type "combo"
           items: [
